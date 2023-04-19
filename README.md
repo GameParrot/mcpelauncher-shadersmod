5c11df51143045ea71468ccbd8d60d35c885c3b5
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-04-19 06:35:01 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# mcpelauncher-shadersmod
+
+This is a mod for mcpelauncher-manifest that allows for RenderDragon shaders.
+
+Usage: 
+
+Put your shaders in a folder called shaders in the mods folder. RenderDragon shaders have an extension of `.material.bin`.  
+
+
+## Building
+`PATH_TO_NDK="/path/to/ndk"`
+- x86
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=x86 ..`
+- x86_64
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=x86_64 ..`
+- armeabi-v7a
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=armeabi-v7a ..`
+- arm64-v8a
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a ..`
---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <sys/mman.h>
+#include <string>
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h> 
+#include <iostream>
+#include <dirent.h>
+#include <sys/types.h>
+#include <vector>
+
+void (*mcpelauncher_preinithook)(const char*sym, void*val, void **orig);
+void (*mcpelauncher_log)(int level, const char* tag, const char* text);
+
+std::string modsDir;
+std::vector<std::string> shadersList;
+
+extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
+    auto h = dlopen("libmcpelauncher_mod.so", 0);
+
+    Dl_info info;
+    dladdr(dlsym(h, "mod_preinit"), &info);
+    modsDir = info.dli_fname;
+
+    modsDir = modsDir.substr(0, modsDir.find_last_of("/"));
+
+      DIR *dr;
+   struct dirent *en;
+   dr = opendir((modsDir + "/shaders").c_str()); //open all directory
+   if (dr) {
+      while ((en = readdir(dr)) != NULL) {
+        if (strstr(en->d_name, ".material.bin")) {
+            shadersList.push_back(std::string(en->d_name));
+        }
+      }
+      closedir(dr); //close all directory
+   }
+
+    mcpelauncher_preinithook = (decltype(mcpelauncher_preinithook)) dlsym(h, "mcpelauncher_preinithook");
+    mcpelauncher_log = (decltype(mcpelauncher_log)) dlsym(h, "mcpelauncher_log");
+
+    mcpelauncher_preinithook("AAssetManager_open", (void*)+[](AAssetManager *mgr, const char *filename, int mode) -> AAsset* {
+        if ((strstr(filename, ".material.bin"))) {
+            std::string fName = std::string(filename).substr(std::string(filename).find_last_of("/") + 1);
+            if (std::find(shadersList.begin(), shadersList.end(), fName) != shadersList.end()) {
+                char pt1[256] = "Patched shader ";
+                strcat(pt1, fName.c_str());
+                strcat(pt1, " via AAssetManager");
+                mcpelauncher_log(0, "ShadersMod", pt1);
+                return AAssetManager_open(mgr, (modsDir + "/shaders/" + fName).c_str(), mode);
+            } else {
+                return AAssetManager_open(mgr, filename, mode);
+            }
+        } else {
+            return AAssetManager_open(mgr, filename, mode);
+        }
+
+
+    }, nullptr);
+
+    mcpelauncher_preinithook("fopen", (void*)+[](const char *filename, const char *mode) -> FILE* {
+        if ((strstr(filename, ".material.bin"))) {
+            std::string fName = std::string(filename).substr(std::string(filename).find_last_of("/") + 1);
+            if (std::find(shadersList.begin(), shadersList.end(), fName) != shadersList.end()) {
+                char pt1[256] = "Patched shader ";
+                strcat(pt1, fName.c_str());
+                strcat(pt1, " via fopen");
+                mcpelauncher_log(0, "ShadersMod", pt1);
+                return fopen((modsDir + "/shaders/" + fName).c_str(), mode);
+            } else {
+                return fopen(filename, mode);
+            }
+        } else {
+            return fopen(filename, mode);
+        }
+    }, nullptr);
+
+    dlclose(h);
+}
+
+extern "C" __attribute__ ((visibility ("default"))) void mod_init() {
+}
