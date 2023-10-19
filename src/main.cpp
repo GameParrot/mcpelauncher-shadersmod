d827e462061e8b70a3a7bd8a0a166af1179c3934
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-10-19 14:58:18 -0400

    fix for 1.20.50

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 #include <string>
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h> 
+#include <android/log.h>
 #include <iostream>
 #include <dirent.h>
 #include <sys/types.h>
 #include <vector>
+#include <unistd.h>
 
 void (*mcpelauncher_preinithook)(const char*sym, void*val, void **orig);
 void (*mcpelauncher_log)(int level, const char* tag, const char* text);
@@ -16,20 +18,54 @@ void (*mcpelauncher_log)(int level, const char* tag, const char* text);
 std::string dataDir = "/data/data/com.mojang.minecraftpe";
 std::vector<std::string> shadersList;
 
+int countCharacterOccurrences(const std::string& str, char character) {
+    int count = 0;
+    for (char c : str) {
+        if (c == character) {
+            count++;
+        }
+    }
+    return count;
+}
+
+std::string assetsToRoot;
+
 extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
+    std::string assetsDir;
+
+    //Below is a hack to get the real path to the assets folder and game data folder
+    char originalDir[PATH_MAX];
+    getcwd(originalDir, sizeof(originalDir));
+    chdir(dataDir.c_str());
+    char currentDir[PATH_MAX];
+    getcwd(currentDir, sizeof(currentDir));
+    chdir(".");
+    char currentDir2[PATH_MAX];
+    getcwd(currentDir2, sizeof(currentDir2));
+    chdir(originalDir);
+    dataDir = currentDir; // gets the data dir using chdir which uses iorewrite 
+    assetsDir = currentDir2; // gets the assets dir using chdir which uses iorewrite
+    chdir("/");
+
+    int occurrences = countCharacterOccurrences(assetsDir, '/');
+
+    for(int i = 0; i<occurrences; i++) {
+        assetsToRoot+="../"; // hack to get back to the root directory using the assets folder as the start due to how the asset makager works
+    }
+
     auto h = dlopen("libmcpelauncher_mod.so", 0);
 
-      DIR *dr;
-   struct dirent *en;
-   dr = opendir((dataDir + "/shaders").c_str()); //open all directory
-   if (dr) {
-      while ((en = readdir(dr)) != NULL) {
-        if (strstr(en->d_name, ".material.bin")) {
-            shadersList.push_back(std::string(en->d_name));
+    DIR *dr;
+    struct dirent *en;
+    dr = opendir((dataDir + "/shaders").c_str()); //open all directory
+    if (dr) {
+        while ((en = readdir(dr)) != NULL) {
+            if (strstr(en->d_name, ".material.bin")) {
+                shadersList.push_back(std::string(en->d_name));
+            }
         }
-      }
-      closedir(dr); //close all directory
-   }
+        closedir(dr); //close all directory
+    }
 
     mcpelauncher_preinithook = (decltype(mcpelauncher_preinithook)) dlsym(h, "mcpelauncher_preinithook");
     mcpelauncher_log = (decltype(mcpelauncher_log)) dlsym(h, "mcpelauncher_log");
@@ -38,35 +74,13 @@ extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
         if ((strstr(filename, ".material.bin"))) {
             std::string fName = std::string(filename).substr(std::string(filename).find_last_of("/") + 1);
             if (std::find(shadersList.begin(), shadersList.end(), fName) != shadersList.end()) {
-                char pt1[256] = "Patched shader ";
-                strcat(pt1, fName.c_str());
-                strcat(pt1, " via AAssetManager");
-                mcpelauncher_log(0, "ShadersMod", pt1);
-                return AAssetManager_open(mgr, (dataDir + "/shaders/" + fName).c_str(), mode);
+                __android_log_print(ANDROID_LOG_VERBOSE, "ShadersMod", "Patched shader %s via AAssetManager", fName.c_str());
+                return AAssetManager_open(mgr, (assetsToRoot + dataDir + "/shaders/" + fName).c_str(), mode); // uses custom asset path like /path/to/assets/../../../ to get to root
             } else {
                 return AAssetManager_open(mgr, filename, mode);
             }
         } else {
             return AAssetManager_open(mgr, filename, mode);
-        }
-
-
-    }, nullptr);
-
-    mcpelauncher_preinithook("fopen", (void*)+[](const char *filename, const char *mode) -> FILE* {
-        if ((strstr(filename, ".material.bin"))) {
-            std::string fName = std::string(filename).substr(std::string(filename).find_last_of("/") + 1);
-            if (std::find(shadersList.begin(), shadersList.end(), fName) != shadersList.end()) {
-                char pt1[256] = "Patched shader ";
-                strcat(pt1, fName.c_str());
-                strcat(pt1, " via fopen");
-                mcpelauncher_log(0, "ShadersMod", pt1);
-                return fopen((dataDir + "/shaders/" + fName).c_str(), mode);
-            } else {
-                return fopen(filename, mode);
-            }
-        } else {
-            return fopen(filename, mode);
         }
     }, nullptr);
 
